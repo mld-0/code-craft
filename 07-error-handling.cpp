a89876594520b800429d0be6d6f4e593cf62eaad
Matthew L Davis <mld.0@protonmail.com>
2022-05-08 22:40:45 +1000

    Error handling, item outline
    Ongoing is the topic of (C style) exceptions-are-banned error handling.

---
+++ 07-error-handling.cpp
@@ -0,0 +1,41 @@
+//	VIM SETTINGS: {{{3
+//	vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
+//	vim: set foldlevel=2 foldcolumn=2:
+//	}}}1
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+//	{{{2
+//	Ongoings:
+//	{{{
+//	Ongoing: error handling when C++ project guidelines say 'no exceptions' (things become C error handling?)
+//	}}}
+
+//	Error handling is a necessity, but if it obsecures logic, it is wrong.
+
+//	Use exceptions rather than return codes
+
+//	Write try-catch-finally statement first
+
+//	Use unchecked exceptions
+
+//	Provide context with exceptions
+
+//	Defien exception class in terms of a caller's needs
+
+//	Define the normal flow
+
+//	Don't return null
+
+//	Don't pass null
+
+
+int main()
+{
+	return 0;
+}
+
+//	Summary:
+//		
+
