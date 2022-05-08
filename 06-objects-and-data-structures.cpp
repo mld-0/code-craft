08cce1693b7f7166abc770385290c3ef3ca85055
Matthew L Davis <mld.0@protonmail.com>
2022-05-08 22:29:42 +1000

    law of demeter, trainwrecks/alternatives
    Chapter presents a few examples of 'bad', not enough examples of
    'do-this-instead', (really need a book on OO best practices, not a
    chapter).
    TODO is a better solution to method chaining (respecting Law-of-demeter,
    but not exploding the number of methods).
    Ongoings: templates-vs-inheritance, and easy-to-remember form of
    law-of-demeter.

--- 06-objects-and-data-structures.cpp
+++ 06-objects-and-data-structures.cpp
@@ -12,9 +12,12 @@ constexpr double pi = 3.141592653589793238463;
 //	{{{
 //	Ongoing: 2022-04-26T00:59:16AEST 'PointOO' best way to only accept initalizer list that must be 2 elements (presumedly <meaning/that-being>, how to reject anything else at compile time?)
 //	}}}
-
+//	TODOs:
+//	{{{
 //	TODO: 2022-04-26T01:01:58AEST clean-code, 06-objects-and-data-structures, (actually implement 'shape' examples (since as we do so, we discover the problem slightly more than trivial), continue with other topics from chapter
 //	TODO: 2022-04-26T00:53:45AEST 'CircleOO_ii, (what is better practice), taking (double, double, double) or (Point, double)? (and it would be best if one could pass '{double,double}, double'
+//	TODO: 2022-05-08T22:13:05AEST clean-code, 06-objects-and-data-structures, (what is the better solution to a design failure like) 'ctxt.getOptions().getScratchDir().getAbsolutePath()'?
+//	}}}
 
 //	Hiding implementation is about abstractions, offering abstract interfaces that allow manipulation of the essence of the data. 
 
@@ -57,17 +60,30 @@ public:
 //	Objects hide their data behind abstractions, and expose functions that operate on that data.
 //	Data-structures expose their data and have no meaningful functions, <leading to> procedural <code/implementations>
 //	The implications:
-//	Procedural code makes it easy to add new functions without changing existing data-structures.
-//	OO code makes it easy to add new classes without changing existing functions.
+//	Procedural code makes it easy to add new functions without changing existing classes, but makes it hard to add new classes since every function must be updated.
+//	OO code makes it easy to add new classes without changing existing functions, but makes it hard to add new functions since every class must be updated.
 //	'Everything-is-an-object' is a myth. Sometimes, the best choice is simple data structures operated on by procedures.
 
 
-//	Data-Structure (Procedural)
-//	Ongoing: 2022-05-08T20:33:32AEST no need to have 'SquareDS', 'RectangleDS' derived from 'ShapeDS'(?)
+class PointOO {
+	double x;
+	double y;
+public:
+	PointOO(double X, double Y) : x(X), y(Y) {}
+	PointOO(initializer_list<double> init) {
+		if (init.size() != 2) { throw invalid_argument("PointOO initializer_list must have size 2"); }
+		x = init.begin()[0];
+		y = init.begin()[1];
+	}
+	PointOO(PointOO&) = default;
+};
 struct PointDS {
 	//	Ongoing: 2022-04-26T01:04:38AEST is <this> multiple-variable declaration not bad (and what is it called?)
 	double x, y;
 };
+
+//	Data-Structure (Procedural)
+//	Allows creation of new functions without changing existing classes.
 struct SquareDS {
 	PointDS topLeft;
 	double side;
@@ -92,18 +108,7 @@ struct GeometryDS {
 
 
 //	Object-oriented implementation
-class PointOO {
-	double x;
-	double y;
-public:
-	PointOO(double X, double Y) : x(X), y(Y) {}
-	PointOO(initializer_list<double> init) {
-		if (init.size() != 2) { throw invalid_argument("PointOO initializer_list must have size 2"); }
-		x = init.begin()[0];
-		y = init.begin()[1];
-	}
-	PointOO(PointOO&) = default;
-};
+//	A class should hide its internal data structure, and expose operations. New classes are easy to add, but adding a new function means every existing class must be updated.
 class SquareOO {
 	PointOO topLeft;
 	double side;
@@ -134,10 +139,14 @@ public:
 //	Ongoing: 2022-04-24T23:26:04AEST here we use <(template instantiation?)> (a compile time check) to ensure all types used with 'GeometryOO::area()' (support the interface) (which is defined by (how we use said type))
 //	Ongoing: 2022-04-24T23:24:22AEST type deduction rules, vis-a-vis template classes vs template member functions
 //	}}}
-//	Parameterized type is used, allows use with any type supporting 'area()' (check is at compile type) [...] (but, using a template class prevents us from creating a single object instance to which any type can be passed (defeating the point)) <(what about using inheritance instead?)>
 struct GeometryOO {
+	//	{{{
+	//	Parameterized type is used, allows use with any type supporting 'area()' (check is at compile type) [...] (but, using a template class prevents us from creating a single object instance to which any type can be passed (defeating the point)) <(what about using inheritance instead?)>
 	//	Ongoing: 2022-04-26T01:25:38AEST necessary(?) to provide both 'T shape' and 'T* shape'
 	//	note: use of template member functions (not a template class)
+	//template<class T>
+	//double area(T& shape) { return shape->area(); }
+	//	}}}
 	template<class T>
 	double area(T shape) { return shape.area(); }
 	template<class T>
@@ -145,6 +154,7 @@ struct GeometryOO {
 	//	we cannot add 'perimeter()' without first implementing it for each shape class
 };
 //	{{{
+//	Ongoing: 2022-05-08T21:19:59AEST (whether templates/inheritance are a better choice, the lesson of OO/DS anti-symmetry is applicable to both?)
 //	Ongoing: 2022-05-08T20:53:11AEST does 'polymorphic' describe use of template functions (or does it <imply/require> inheritance/virtual-functions)?
 //	Ongoing: 2022-05-08T20:40:39AEST C++, best practices, using templates for 'GeometryOO' (vs using inheritance/virtual-functions)?
 //	Ongoing: 2022-04-24T23:29:58AEST Sort of defeats the point does it not? (use of template class results in an instance that can only be used for one of our shape types) (and having to move a unique pointer (and then the *nightmare* of having to return it again) (atomic that?))
@@ -155,8 +165,43 @@ struct GeometryOO {
 //	}}}
 
 
-//	Law of Demeter:
+//	Hybrids: classes that are partial OO and partially DS (use functions to perform significant tasks, but make class variables accessible), (make it hard to add both new data, and hard to add new functions) - indicative of a muddled design, avoid.
 
+//	Ongoing: 2022-05-08T21:53:49AEST 'law of demeter' (but in an intuitive/easy-to-understand form)
+//	Law of Demeter: A module should not know about the innards of the object it manipulates.
+//	A method 'f' of class 'Widget' whould only call these methods:
+//		Methods belonging to 'Widget'
+//		An object created <by/inside?> 'f'
+//		An object passed as argument to 'f'
+//		An object held in an instance variable of 'Widget'
+//	(not methods of objects that are returned by any of these functions) (talk to friends, not strangers)
+//	(common violation: 'train-wrecks') (whether example below violates Law of Demeter depends on whether ctxt/opts/scratchDir are objects (yes) or data-structures (no)).
+
+//	LINK: http://randomthoughtsonjavaprogramming.blogspot.com/2013/10/trainwreck-vs-method-chaining.html
+//	Train-wreck: <(a chain of methods with returned objects of different types)>
+//			final String outputDir = ctxt.getOptions().getScratchDir().getAbsolutePath()
+//	Better written as:
+//			Options opts = ctxt.getOptions();
+//			File scratchDir = opts.getScratchDir();
+//			final String outputDir = scratchDir.getAbsolutePath();
+//	Method-chaining: <(a chain of methods each returning the same type of object)> (not necessarily (but probably) bad?)
+
+
+//	Hiding structure: (consider)
+//			ctxt.getAbsolutePathOfScratchDir()
+//	vs
+//			ctxt.getScratchDir().getAbsolutePath()
+//	The first approach is likely to lead to an explosion in the number of methods. <(The second presumes 'getScratchDir()' returns a data strucutre (instead of an object)?)>. (Both are unappealing options).
+//	instead:
+//			<(?)>
+
+//	Expression-builder: <(A seperate class, which hides the underlying method-chaining to create an object, providing a single method for each combination of arguments)>
+
+//	Fluent-Interface: <(Interface built on method chaining)>
+//	LINK: https://martinfowler.com/bliki/FluentInterface.html
+
+//	Data-Transfer-Object: a class with public variables and no functions
+//	Active-Record-Object: Hybrid form of data-transfer-object, provides navigational methods like 'save()' and 'find()' (bad: use a seperate object containing this logic)
 
 
 //	OO_i, OO_ii examples (?):
@@ -255,4 +300,8 @@ int main()
 	return 0;
 }
 
+//	Summary:
+//		Objects expose behaviour and hide data. This makes it easy to add new kinds of objects, but difficult to add new features/behaviour.
+//		Data structures expose data and have no significant behaviour. This makes it easy to add new features/behaviour, but difficult to add new kinds of data-structures.
+//		'Everything-is-an-object' is a myth, choose the best option for a given task.
 
