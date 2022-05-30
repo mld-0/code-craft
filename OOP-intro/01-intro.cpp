0893abd1214e48e4b64a80ee34da58383a4a4d50
Matthew L Davis <mld.0@protonmail.com>
2022-05-31 01:15:52 +1000

    review, cleanup
    Continue: finish book chapter.

--- OOP-intro/01-intro.cpp
+++ OOP-intro/01-intro.cpp
@@ -7,7 +7,13 @@
 #include <string>
 using namespace std;
 //	{{{2
+//	Ongoings:
+//	{{{
+//	Ongoing: 2022-05-31T01:08:31AEST seeing the 'hierarchy of class implementation' (pre-perquisite classes for a class) where inheritance is not used (nicely listing them all in one place when using composition rather than inheritance) [...] (and inheritance vs composition (generally) re: said example)
+//	}}}
 
+//	Background:
+//	{{{
 //	Other Programming Paradigms:
 //		Imperative (Pascal, C)
 //		Logic (Prolog)
@@ -22,7 +28,7 @@ using namespace std;
 //	It is also tends to lead to the 'If your tool is a Hammer' fallacy.
 //	<(Everything is an object (is a lie?))>
 
-//	Example: DNA sequence as a vector of N integers, discover whether any pattern of length M is ever repeated.
+//	Example: (switching to a different paradigm leading to a better solution) DNA sequence as a vector of N integers, discover whether any pattern of length M is ever repeated.
 //	{{{
 //	O(M * N**2):
 //	     DO 10 I = 1, N-M
@@ -43,35 +49,43 @@ using namespace std;
 //	Sapir-Whorf hypothesis: it may be possible for an individual working in one language to utter ideas that cannot be translated (or even understood) by individuals working in another <linguistic-framework>.
 //	<(A question of ideas vs implementation?)>
 //	The choice of language will *direct* thoughts, but it cannot *proscribe* (inhibit/prevent) thoughts
+//	}}}
+
+//	An object will normally carry out an action by delegating-to/calling-on other objects. 
+//	This is a natural fit for the kind of re-use that needed for modern software development.
+//	A key aspect of OO-programs is information hiding - objects make an interface available to callers and keep their implementation hidden.
+
+//	Example: hierarchy of class implementation
+//		RobinFlorist: DeliveryPerson, FlowerAranger, Wholesaler
+//			DeliveryPerson: Robin
+//				Robin: Person
+//			Wholesaler: Grower
+//				Grower: Gardeners
 
 
 //	Agents and communities:
-//	An object-oriented program is structured around A community of objects interacting by making requests.
-//			agent: an object
-//			message: <(argument? other methods of passing information to an object)>
-//			method: <(implementation, details of which are typically hidden from caller (and may involve delegations to other classes in the hierarchy of the <problem/solution-space>?))>
-//	{{{
-//	Example: classes used to implement each class
-//		RobinFlorist: DeliveryPerson, FlowerAranger, Wholsaler
-//				DeliveryPerson: Robin
-//				Wholesaler: Grower
-//						Grower: Gardeners
-//	}}}
-
-//	Message and methods
-//		Action is initiated through transmission of message to 'agent' (object) responsible for action.
-//		Message encodes request for action, is accompanied by additional information needed to carry out request
-//		Message is sent to 'receiver', who accepts responsibility and carries out the action.
-//		An object will normally carry out an action by delegating to other objects. This <supports/is-conducive> to the kind of software re-use that is a vital to modern software development.
+//	OO-programs are structured around a community of objects interacting by making requests.
+//			Agent: an object
+//			Action: initiated through transmission of message to 'agent' (object) responsible for action.
+//			Message: a request for a method, accompanied by additional information needed to carry out request
+//			(Designated) Receiver: accepts responsibility for message, carries out the requested action
+//			Method: responsible for carrying out the requested action
 
 
 //	Message vs Procedural call:
+//	{{{
+//	wat?
 //		A message (calling the method of an object) has a designated receiver
 //		The interpretation of the method is determined by the receiver, and may be different for different receivers.
-//	<(the specific receiver of a message <is/will> <usually/often/sometimes?> not be known until runtime? (necessitating 'late-binding') (<when> using an interpreted language, or pointer) (this is a C++-centric is it not?))>
+//	<(the specific receiver of a message <is/will> <usually/often/sometimes?> not be known until runtime? (necessitating 'late-binding') (<when> using an interpreted language, or pointer) (this is a C++-centric description is it not?))>
+//	}}}
+//	A message (method call) has a designated receiver, whereas a procedure call does not.
+//	The interpretation of the message is determined by the receiver (different classes can define different versions of the same function).
+//	<(There is late-binding between the message (object method call) and the code invoked (vs early-binding for a procedure call))>
 
 
 //	Responsibilities:
+//		
 
 
 
