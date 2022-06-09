703d08ee65439f487862f3849c299ba6687623b9
Matthew L Davis <mld.0@protonmail.com>
2022-06-10 00:52:42 +1000

    intro, principles of clean code

---
+++ clean-code/01-intro.py
@@ -0,0 +1,23 @@
+#   vim: set tabstop=4 modeline modelines=10:
+#   vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
+import sys
+import os
+import logging
+#   {{{2
+
+#   Code is the specification of program requirements in a machine-executable format.
+#   Code is susceptible to a LeBlanc's Law: 'Later equals never'
+#   Bad code kills productivity. Bad code encourages bad code.
+
+#   Code is read far more than it is written.
+
+#   Principles of clean code:
+#       Elegance and efficient, makes bugs hard to hide. 
+#       Bad code tries to do too much. Clean code does one thing well, things are undistracted by surrounding details.
+#       Reads like prose. Has meaningful names. Lacks unnecessary detail.
+#       Has (passing) unit/acceptance tests.
+#       Contains no duplication.
+#       Is simple, obvious, and compelling.
+
+#   The boyscout rule: leave the campsite cleaner than you found it.
+
