42aa496d6508692785b3c403fe725a7aace681ff
mld-0 <mld.0@protonmail.com>
2023-03-26 22:54:28 +1100

    6.4

--- object-design-style-guide/06-query-methods.py
+++ object-design-style-guide/06-query-methods.py
@@ -6,9 +6,12 @@ from __future__ import annotations
 import sys
 import os
 import unittest
+from dataclasses import dataclass
 #   Notes:
 #   {{{
 #   2023-03-26T21:25:16AEDT book uses 'modifier method' to refer specifically to methods to return a modified copy of an immutable class (this is not the wider definition of 'modifier method'?)
+#   2023-03-26T22:36:53AEDT how does 6.4 CurrencyConverter go as far as being a well designed set of classes? [...] (the fact 'money.converted(exchangeRate)' requires 'exchangeRate.currency_from' to match the currency of 'money' feels like an indicator this design could be better) [...] (if nothing else, that's a lot of lines to do not much)
+#   2023-03-26T22:41:49AEDT 'CounterImmutable' -> (going by the lessons of the book, the ctor should require an argument for 'count', and the creation of a count=0 object should be handled by a factory function) (book is yet to mention default arguments - (java doesn't have them?))
 #   }}}
 
 #   6.1) Use query methods for information retrieval for mutable objects
@@ -24,9 +27,9 @@ class CounterMutable:
         self.count += 1
 
 #   For immutable classes, command methods instead return a modified copy of the object
+@dataclass(frozen=True)
 class CounterImmutable:
-    def __init__(self):
-        self.count = 0
+    count: int = 0
     def incremented(self) -> CounterImmutable:
         return CounterImmutable(self.count + 1)
     def currentCount(self) -> int:
@@ -73,4 +76,61 @@ t.test_CounterImmutable()
 
 
 #   6.4) Define specific methods and types for the queries you want to make
+#   <>
+#   {{{
+#class Currency:
+#    def __init__(self, code):
+#        self.code = code
+#    def __eq__(self, lhs):
+#        return self.code == lhs.code
+#class Money:
+#    def __init__(self, amount: int, currency: Currency):
+#        self.amount = amount
+#        self.currency = Currency
+#    def converted(self, exchangeRate: ExchangeRate):
+#        if not exchangeRate.currency_from == self.currency:
+#            raise Exception("currency of exchangeRate does not match")
+#        return Money(self.amount * exchangeRate.rate, exchangeRate.currency_to)
+#class ExchangeRate:
+#    def __init__(self, currency_from: Currency, currency_to: Currency, rate: float):
+#        self.currency_from = currency_from
+#        self.currency_to = currency_to
+#        self.rate = rate
+#class FixerAPI:
+#    def exchangeRateFor(currency_from: Currency, currency_to: Currency) -> ExchangeRate:
+#        rate = 1.0
+#        return ExchangeRate(currency_from, currency_to, rate)
+#class CurrencyConverter:
+#    def __init__(self, fixerAPI: FixerAPI):
+#        self.fixerAPI = fixerAPI
+#    def convert(self, money: Money, currency_to: Currency) -> Money:
+#        exchangeRate = self.fixerAPI.exchangeRateFor(money.currency, currency_to)
+#        return money.converted(exchangeRate)
+#   }}}
+
+
+#   6.5) Define an abstraction for queries that cross system boundaries
+#   Any call across a system boundry should be hidden behind an abstraction which:
+#           Uses a service interface instead of a service class
+#           Leaves out implementation details
+#
+#   Abstractions make it possible to create test scenarios without making actual network/filesytem calls
+#   It also provides us with freedom to change implementation details at a later point
+#
+#   <>
+
+
+#   Python and interface classes
+#   {{{
+#   }}}
+
+
+#   6.6) Use stubs for test doubles with query methods
+
+
+#   6.7) Query methods should use other query methods, not command methods
+
+
+#   Summary:
+#   <>
 
