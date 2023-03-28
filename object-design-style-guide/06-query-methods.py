f851a1d2a77603a4ec572511217dde546f3ea35f
mld-0 <mld.0@protonmail.com>
2023-03-28 22:30:55 +1100

    6.6-6.7 and summary

--- object-design-style-guide/06-query-methods.py
+++ object-design-style-guide/06-query-methods.py
@@ -12,6 +12,9 @@ from dataclasses import dataclass
 #   2023-03-26T21:25:16AEDT book uses 'modifier method' to refer specifically to methods to return a modified copy of an immutable class (this is not the wider definition of 'modifier method'?)
 #   2023-03-26T22:36:53AEDT how does 6.4 CurrencyConverter go as far as being a well designed set of classes? [...] (the fact 'money.converted(exchangeRate)' requires 'exchangeRate.currency_from' to match the currency of 'money' feels like an indicator this design could be better) [...] (if nothing else, that's a lot of lines to do not much)
 #   2023-03-26T22:41:49AEDT 'CounterImmutable' -> (going by the lessons of the book, the ctor should require an argument for 'count', and the creation of a count=0 object should be handled by a factory function) (book is yet to mention default arguments - (java doesn't have them?))
+#   2023-03-28T21:56:10AEDT mock test objects is really a topic for a whole item
+#   2023-03-28T22:22:13AEDT 'InvoicePersistance' is a terrible name for a class for saving Invoices to a file(?)
+#   2023-03-28T22:26:29AEDT abstract vs interface class?
 #   }}}
 
 #   6.1) Use query methods for information retrieval for mutable objects
@@ -62,7 +65,7 @@ t.test_CounterImmutable()
 #   Alternatively, use a Result or other type that can represent the null/None case
 #   (whichever option is chosen, the client will have to handle the possibility of a null/None case)
 #   
-#   Use 'getX' to indicate query methods that cannot fail and 'findX' to indicate those that may
+#   Use names like 'getX' to indicate query methods that cannot fail and 'findX' to indicate those that may
 
 
 #   6.3) Avoid query methods that expose internal state
@@ -116,21 +119,72 @@ t.test_CounterImmutable()
 #
 #   Abstractions make it possible to create test scenarios without making actual network/filesytem calls
 #   It also provides us with freedom to change implementation details at a later point
+
+
+#   SOLID principles:
+#   {{{
+#   LINK: https://www.freecodecamp.org/news/solid-principles-explained-in-plain-english/
+#   LINK: https://www.digitalocean.com/community/conceptual-articles/s-o-l-i-d-the-first-five-principles-of-object-oriented-design
+
+#   Single responsibility principle
+#   A class should do one thing, and therefore it should have only a single reason to change
+#   (There should only be one aspect of the software's specification which, if changed, would require a class to be changed)
+#   Avoid mixing business/persistence logic
+#
+#   An 'Invoice' class should not be responsible for saving an invoice to a file - define an 'InvoicePersistance' class to handle this
+
+
+#   Open-closed principle
+#   Classes should be open for extension and closed to modification
+#   (We should be able to add new functionality without altering the existing class)
+#   
+#   Use Interface classes
+#   Define an 'InvoicePersistance' abstract class, and create separate 'FilePersistence' and 'DatabasePersistence' to handle saving to files/databases respectively
 #
 #   <>
 
 
-#   Python and interface classes
+#   Liskov substitution principle
+#   Subclasses should be substitutable for their base class
+#   <>
+
+
+#   Interface segregation principle
+#   <>
+
+
+#   Dependency inversion principle
+#   <>
+
+#   }}}
+
+
+#   Python and interface classes:
 #   {{{
 #   }}}
 
 
-#   6.6) Use stubs for test doubles with query methods
+#   6.6) Use stubs (mock objects) for test doubles with query methods
+#   A stub is a mock test object which returns hardcoded values
+#   Separate testing of logic which uses external values from the fetching of those values
+#   (A test which relies on the outside world is an integration test, not a unit test)
+#   contention: don't use mocking tools for creating stubs, manually created objects will be easier to use and maintain
 
 
 #   6.7) Query methods should use other query methods, not command methods
+#   Query methods should not have side effects, while command methods may
+#   Consider separating any query method with side effects into two methods
+
+#   Exceptions/alternatives to command-query separation
+#   LINK: https://blog.ploeh.dk/2014/08/11/cqs-versus-server-generated-ids/
+#   {{{
+#   }}}
 
 
 #   Summary:
-#   <>
+#   A query method retrieves a piece of information. They should have a single return type (and consider alternatives to returning null/None). 
+#   Query methods should not have side effects, and should expose as little of an objects internals as possible.
+#   Define specific query methods for each question you want to ask.
+#   Define an abstraction wherever queries cross system boundaries
+#   Use mock objects when testing code that relies on queries that cross system boundaries.
 
