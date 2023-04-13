6f8f04bef83a419862c6def32b2e5e256f2b58b8
mld-0 <mld.0@protonmail.com>
2023-04-13 22:56:40 +1000

    capture/validate ctor output
    Create function 'check_stdout', call value with *args/**kwargs and
    assert resulting stdout matches given string.

--- object-design-style-guide/extra/inheritance.py
+++ object-design-style-guide/extra/inheritance.py
@@ -2,23 +2,41 @@
 #   vim: set tabstop=4 modeline modelines=10:
 #   vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
 #   {{{2
+import sys
+import io
+from contextlib import redirect_stdout
 #   Notes:
 #   {{{
 #   2023-04-05T19:35:00AEST instead of printing A B C - capture output and assert it against expected value
 #   2023-04-05T19:35:28AEST behaviour of all examples same for different python versions?
 #   2023-04-13T21:01:19AEST traits -> see effective-rust(?)
+#   2023-04-13T22:40:36AEST 'check_stdout()' is in SnippetsLab (plz update on change)
 #   }}}
+
+def check_stdout(obj, check, *args, **kwargs):
+    """Validate stdout from 'x = obj(*args, **kwargs)' matches 'check' verification"""
+    #   {{{
+    buffer = io.StringIO()
+    with redirect_stdout(buffer):
+        x = obj(*args, **kwargs)
+    result = buffer.getvalue().strip()
+    if not result == check:
+        print(f"error, result=({result}), check=({check})")
+        exit(2)
+    print(result)
+    #   }}}
+
+
+#   Python handles multiple-inheritance badly
 
 #   calling 'super()'
 #   Calls to base class ctors can be made as 'A.__init__(self)' or 'super().__init__()'
 #   (the former does not allow for dependency injection; use 'super()' instead)
 #   (use 'A.foo()' instead of 'super()' to explicitly access methods of 'A')
 
-
-
-#   Python handles multiple-inheritance badly
-
-#   'super()' is equivalent to 'super(type(self), self)'
+#   'super()' 
+#           is equivalent to:
+#   'super(type(self), self)'
 
 #   'super(Foo, self).method()'
 #   call next 'method()' in the method resolution order that comes after class 'Foo'
@@ -29,35 +47,36 @@
 def _multi_inheritance_1():
     class A:
         def __init__(self):
-            print("A, ", end="")
+            print("A", end="|")
     class B:
         def __init__(self):
-            print("B, ", end="")
+            print("B", end="|")
 
-    #   correct: 'A B C1'
+    #   correct: 'A|B|C1'
     class C1(A, B):
         def __init__(self):
             super().__init__()              #   calls A.__init__()  (equivalent to 'super(C1, self)')
             super(A, self).__init__()       #   calls B.__init__()
-            print("C1, ", end="")
+            print("C1", end="")
 
-    #   incorrect: 'B C2'
+    #   incorrect: 'B|C3'
     class C3(A, B):
         def __init__(self):
             super(A, self).__init__()       #   calls B.__init__()
             super(B, self).__init__()       #   calls nothing
-            print("C3, ", end="")
+            print("C3", end="")
 
-    #   incorrect: 'A C4'
+    #   incorrect: 'A|C4'
     class C4(A, B):
         def __init__(self):
-            super().__init__()
-            print("C4, ", end="")
+            super().__init__()              #   calls only first base class ctor
+            print("C4", end="")
 
-    c = C1(); print();
-    c = C3(); print();
-    c = C4(); print();
+    check_stdout(C1, "A|B|C1")
+    check_stdout(C3, "B|C3")
+    check_stdout(C4, "A|C4")
     print()
+
 _multi_inheritance_1()
 
 
@@ -67,27 +86,28 @@ def _multi_inheritance_2():
     class A:
         def __init__(self):
             super().__init__()
-            print("A, ", end="")
+            print("A", end="|")
     class B:
         def __init__(self):
             super().__init__()
-            print("B, ", end="")
+            print("B", end="|")
 
-    #   correct: 'B A C'
+    #   correct: 'B|A|C1'
     class C1(A, B):
         def __init__(self):
             super().__init__()
-            print("C, ", end="")
+            print("C1", end="")
 
-    #   correct: 'A B C'
+    #   correct: 'A|B|C2'
     class C2(B, A):
         def __init__(self):
             super().__init__()
-            print("C, ", end="")
+            print("C2", end="")
 
-    c = C1(); print();
-    c = C2(); print()
+    check_stdout(C1, "B|A|C1")
+    check_stdout(C2, "A|B|C2")
     print()
+
 _multi_inheritance_2()
 
 
@@ -95,23 +115,30 @@ _multi_inheritance_2()
 def _multi_inheritance_handling_args():
     class A:
         def __init__(self, a):
-            print(f"A({a}), ", end="")
+            print(f"A({a})", end="|")
     class B(A):
         def __init__(self, b, *args, **kwargs):
             super().__init__(*args, **kwargs)
-            print(f"B({b}), ", end="")
+            print(f"B({b})", end="|")
     class C(A):
         def __init__(self, c, *args, **kwargs):
             super().__init__(*args, **kwargs)
-            print(f"C({c}), ", end="")
-    class D(B, C):
+            print(f"C({c})", end="|")
+    class D1(B, C):
         def __init__(self, d, *args, **kwargs):
             super().__init__(*args, **kwargs)
-            print(f"D({d}), ", end="")
+            print(f"D1({d})", end="")
+    class D2(C, B):
+        def __init__(self, d, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            print(f"D2({d})", end="")
 
-    d = D(1, 2, 3, 4); print()
-    d = D(a=4, b=3, c=2, d=1); print()
+    check_stdout(D1, "A(4)|C(3)|B(2)|D1(1)", 1, 2, 3, 4)             #   D1(1, 2, 3, 4)
+    check_stdout(D2, "A(4)|B(3)|C(2)|D2(1)", 1, 2, 3, 4)             #   D2(1, 2, 3, 4)
+    check_stdout(D1, "A(4)|C(2)|B(3)|D1(1)", a=4, b=3, c=2, d=1)     #   D1(a=4, b=3, c=2, d=1)
+    check_stdout(D2, "A(4)|B(3)|C(2)|D2(1)", a=4, b=3, c=2, d=1)     #   D2(a=4, b=3, c=2, d=1)
     print()
+
 _multi_inheritance_handling_args()
 
 
@@ -125,6 +152,7 @@ _multi_inheritance_handling_args()
 #   Traits allow default implementations <(interface classes often do not?)>
 #   <(traits do not modify the class hierachy - presumedly refering to langauge other than python)>
 #   Traits are often used to encapsulate common behaviour for reuse - logging, error handling, or serializaiton
+#   <>
 
 def _printable_trait():
 
@@ -136,15 +164,17 @@ def _printable_trait():
             self.name = name
             self.age = age
         def __repr__(self):
-            return f"{self.name}, {self.age}"
+            return f"I am {self.name}, age {self.age}"
 
     person = Person("John Doe", 30)
-    person.print()
+    check_stdout(person.print, "I am John Doe, age 30")
 
 _printable_trait()
 
 
 #   mixins are like traits, but typically rely on composition instead of inheritance
+#   <>
+
 def _printable_mixin():
 
     class PrintableMixin:
@@ -156,7 +186,7 @@ def _printable_mixin():
             self.name = name
             self.age = age
         def __repr__(self):
-            return f"{self.name}, {self.age}"
+            return f"I am {self.name}, age {self.age}"
 
     class PrintablePerson(PrintableMixin):
         def __init__(self, person):
@@ -166,7 +196,7 @@ def _printable_mixin():
 
     person = Person("John Doe", 30)
     printable_person = PrintablePerson(person)
-    printable_person.print() 
+    check_stdout(printable_person.print, "I am John Doe, age 30")
 
 _printable_mixin()
 
