c9e1616dc4f1b845de87c05eab59e56947696ed2
mld-0 <mld.0@protonmail.com>
2023-06-28 18:56:35 +1000

    leetcode, oop-top-interview-questions (empty)

---
+++ _empty/oop-top-interview-questions.java
@@ -0,0 +1,1 @@
+../leetcode/oop-top-interview-questions.java
---
+++ leetcode/oop-top-interview-questions.java
@@ -0,0 +1,11 @@
+//	{{{3
+//	vim: set tabstop=4 modeline modelines=10:
+//	vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
+//	{{{2
+
+//	Questions:
+//	LINK: https://leetcode.com/discuss/interview-question/object-oriented-design/3655541/Top-OOPs-Interview-Questions-in-Java-with-Answers
+
+//	Others:
+//	LINK: https://leetcode.com/discuss/interview-question/object-oriented-design/?currentPage=1&orderBy=hot&query=
+
