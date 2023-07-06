d67c102743605eff4c0f142e954f1334878a0ef3
mld-0 <mld.0@protonmail.com>
2023-07-06 22:10:44 +1000

    rename as per Java rules, qus 1-2

---
+++ leetcode/OOPTopInterviewQuestions.java
@@ -0,0 +1,53 @@
+//	{{{3
+//	vim: set tabstop=4 modeline modelines=10:
+//	vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
+//	{{{2
+
+//	Running Java file: 
+//		>$	javac OOPTopInterviewQuestions.java
+//		>$	java OOPTopInterviewQuestions
+
+//	Questions:
+//	LINK: https://leetcode.com/discuss/interview-question/object-oriented-design/3655541/Top-OOPs-Interview-Questions-in-Java-with-Answers
+//	Others:
+//	LINK: https://leetcode.com/discuss/interview-question/object-oriented-design/?currentPage=1&orderBy=hot&query=
+
+public class OOPTopInterviewQuestions {
+
+	static void q1() {
+	//	What are the core OOP concepts in Java:
+	//	Or, What are the basic principles of OOPs concept in Java?
+	//	Or, What is the main fundamental principles (main pillars) of the OOPs concepts in Java?
+
+	//	1)	Encapsulation: the mechanism of hiding data implementations by restricting access to public methods
+	//
+	//	2)	Abstraction: a technique where we hide data not required by the user
+	//
+	//	3)	Inheritance: a process where a child class inherits all the attributes of its parent
+	//
+	//	4)	Polymorphism: A technique by which we can perform a single task in different ways
+	//	(Accessing different types of objects through the same interface)
+	//	(An object is polymorphic if it passes multiple is-a `instanceof` tests)
+	//	(All Java objects are technically polymorphic, since all Java classes inherit/extend 'Object')
+	//	Kinds of polymorphism:
+	//			Static (compile-time) polymorphism: method overloading
+	//			Dynamic polymorphism: method overriding
+	}
+
+	static void q2() {
+	//	What is OOP in Java:
+
+	//	Object-oriented-programming is a methodology (or paradigm) to design a program using classes an objects
+	//	[{the most popular methodology in use today (I mean, what popular modern languages don't have objects?}]
+	//	[{an extension of procedural programming}]
+	}
+
+	public static void main(String[] args)
+	{
+		q1();
+		q2();
+		System.out.println("Done");
+	}
+
+}
+
