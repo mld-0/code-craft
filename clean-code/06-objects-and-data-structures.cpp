d6f36a81e8afb578003777bdac359ccd75049d8b
mld-0 <mld.0@protonmail.com>
2023-08-08 20:44:24 +1000

    law of demeter example

--- clean-code/06-objects-and-data-structures.cpp
+++ clean-code/06-objects-and-data-structures.cpp
@@ -19,7 +19,9 @@ constexpr double pi = 3.141592653589793238463;
 //	Ongoing: 2022-05-08T20:53:11AEST GeometryOO, does 'polymorphic' describe use of template functions (or does it <imply/require> inheritance/virtual-functions)?
 //	Ongoing: 2022-05-08T20:40:39AEST GeometryOO, C++, best practices, using templates for 'GeometryOO' (vs using inheritance/virtual-functions)?
 //	Ongoing: 2022-04-24T23:29:58AEST GeometryOO, Sort of defeats the point does it not? (use of template class results in an instance that can only be used for one of our shape types) (and having to move a unique pointer (and then the *nightmare* of having to return it again) (atomic that?))
+//	Ongoing: 2022-05-08T21:53:49AEST 'law of demeter' (but in an intuitive/easy-to-understand form)
 //	}}}
+
 //	TODO: 2022-04-26T01:01:58AEST clean-code, 06-objects-and-data-structures, (actually implement 'shape' examples (since as we do so, we discover the problem slightly more than trivial), continue with other topics from chapter
 //	TODO: 2022-04-26T00:53:45AEST 'CircleOO_ii, (what is better practice), taking (double, double, double) or (Point, double)? (and it would be best if one could pass '{double,double}, double'
 //	TODO: 2022-05-08T22:13:05AEST clean-code, 06-objects-and-data-structures, (what is the better solution to a design failure like) 'ctxt.getOptions().getScratchDir().getAbsolutePath()'?
@@ -82,6 +84,7 @@ public:
 //	Data-structures expose their data and have no meaningful functions, <leading to> procedural <code/implementations>
 //	'Everything-is-an-object' is a myth. Sometimes, the best choice is simple data structures operated on by procedures.
 
+
 //	Data-Structure: 
 //	Hard to add classes, easy to add functions.
 struct RectangleDS {
@@ -129,11 +132,19 @@ struct GeometryOO {
 	//	we cannot add 'perimeter()' without first implementing it for each shape class
 };
 
-//	Hybrids: classes that are partial OO and partially DS (use functions to perform significant tasks, but make class variables accessible), (make it hard to add both new data, and hard to add new functions) - indicative of a muddled design, avoid.
+
+//	Hybrids: classes that are partial Objects and partially Data-structures
+//	(uses functions to perform significant tasks, but makes class variables accessible)
+//	(Combines downsides of both data-structures and objects - hard to add new data, and hard to add new methods) 
+//	indicative of a muddled design, avoid.
 
 
-//	Ongoing: 2022-05-08T21:53:49AEST 'law of demeter' (but in an intuitive/easy-to-understand form)
-//	Law of Demeter: A module should not know about the innards of the object it manipulates.
+//	Law of Demeter: 
+//	If A owns B, and B owns C, A should use B to manipulate C instead of trying to do so itself
+//	A module should not know about the innards of the object it manipulates.
+//	A method should only call the methods of objects passed to it as arguments, or that are fields of the class it belongs to
+//	Don't talk to strangers: avoid method chaining (calling methods of objects returned by other methods)
+//	{{{
 //	A method 'f' of class 'Widget' whould only call these methods:
 //		Methods belonging to 'Widget'
 //		An object created <by/inside?> 'f'
@@ -141,16 +152,57 @@ struct GeometryOO {
 //		An object held in an instance variable of 'Widget'
 //	(not methods of objects that are returned by any of these functions) (talk to friends, not strangers)
 //	(common violation: 'train-wrecks') (whether example below violates Law of Demeter depends on whether ctxt/opts/scratchDir are objects (yes) or data-structures (no)).
+//	}}}
+//	Example: `Store` does not access methods/fields of `Wallet`, but instead only uses the interface provided by `Person`
+class Wallet {
+public:
+	Wallet(int amount) : _amount(amount) {}
+	bool debit(int amount) {
+		if (_amount >= amount) {
+			_amount -= amount;
+			return true;
+		}
+		return false;
+	}
+	int get_balance() const { return _amount; }
+private:
+	int _amount;
+};
+class Person {
+public:
+	Person() : wallet(100) {}
+	int show_balance() const { return wallet.get_balance(); }
+	bool make_payment(int amount) { return wallet.debit(amount); }
+private:
+	Wallet wallet;
+};
+class Store {
+public:
+	string purchase(Person& person, int cost) {
+		if (person.make_payment(cost)) {
+			return "Purchased";
+		} 
+		return "Declined";
+	}
+};
 
 
+//	Train-wreck: 
 //	LINK: http://randomthoughtsonjavaprogramming.blogspot.com/2013/10/trainwreck-vs-method-chaining.html
-//	Train-wreck: <(a chain of methods with returned objects of different types)>
+///	A chain of methods with returned objects of different types
 //			final String outputDir = ctxt.getOptions().getScratchDir().getAbsolutePath()
-//	Better written as:
+//	This Better written as:
 //			Options opts = ctxt.getOptions();
 //			File scratchDir = opts.getScratchDir();
 //			final String outputDir = scratchDir.getAbsolutePath();
-//	Method-chaining: <(a chain of methods each returning the same type of object)> (not necessarily (but probably) bad?)
+//
+//	Fluent interface:
+//			Person larry = (new Person()).setName("larry").setJob("Chief Mouser");
+//	A chain of methods with returned objects of the same type does not break the law of demeter
+//	(This can be an alternative to resorting to ctors with many arguments, although in this case an expression builder may be a better solution)
+//
+//	Expression-builder: 
+//	A seperate class, which hides the underlying method-chaining to create an object, providing a single method for each combination of arguments
 
 
 //	Hiding structure: (consider)
@@ -158,21 +210,25 @@ struct GeometryOO {
 //	vs
 //			ctxt.getScratchDir().getAbsolutePath()
 //	The first approach is likely to lead to an explosion in the number of methods. <(The second presumes 'getScratchDir()' returns a data strucutre (instead of an object)?)>. (Both are unappealing options).
-//	instead:
-//			<>
+//	Instead: consider what the caller is likely trying to do: read a file in the scratch-dir
+//	So, make `getAbsolutePathOfScratchDir()` a private method, and provide a public method `createScratchFileStream()` which handles creating/opening the file without requiring the caller to handle details like the path of the scratch-dir
 
 
-//	Expression-builder: <(A seperate class, which hides the underlying method-chaining to create an object, providing a single method for each combination of arguments)>
 
-
-//	Fluent-Interface: <(Interface built on method chaining)>
+//	Fluent-Interface: 
 //	LINK: https://martinfowler.com/bliki/FluentInterface.html
+//	[{Interface built on method chaining}]
 //	{{{
 //	}}}
 
 
 //	Data-Transfer-Object: a class with public variables and no functions
 //	Active-Record-Object: Hybrid form of data-transfer-object, provides navigational methods like 'save()' and 'find()' (bad: use a seperate object containing this logic)
+
+
+//	LINK: https://medium.com/vattenfall-tech/the-oop-has-been-explained-wrongly-to-me-db8e36f91bb2
+//	{{{
+//	}}}
 
 
 //	Resources:
@@ -194,8 +250,9 @@ int main()
 }
 
 //	Summary:
-//		Objects expose behaviour and hide data. This makes it easy to add new kinds of objects, but difficult to add new features/behaviour.
-//		Data structures expose data and have no significant behaviour. This makes it easy to add new features/behaviour, but difficult to add new kinds of data-structures.
-//		'Everything-is-an-object' is a myth, choose the best option for a given task.
+//	Objects expose behaviour and hide data. This makes it easy to add new kinds of objects, but difficult to add new features/behaviour.
+//	Data structures expose data and have no significant behaviour. This makes it easy to add new features/behaviour, but difficult to add new kinds of data-structures.
+//	'Everything-is-an-object' is a myth, choose the best option for a given task.
+//	If A owns B, and B owns C, A should use B to manipulate C instead of trying to do so itself
 //		<>
 
