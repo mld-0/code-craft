b5899e8fcb6eb719af859e5ceb3fbe70ec679d70
mld-0 <mld.0@protonmail.com>
2023-08-09 15:58:17 +1000

    ongoing

--- clean-code/07-error-handling.cpp
+++ clean-code/07-error-handling.cpp
@@ -1,35 +1,11 @@
 //	VIM SETTINGS: {{{3
 //	vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
 //	vim: set foldlevel=2 foldcolumn=2:
-//	}}}1
+//	{{{2
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
-//	{{{2
-//	Ongoings:
-//	{{{
-//	Ongoing: error handling when C++ project guidelines say 'no exceptions' (things become C error handling?)
-//	}}}
-
-//	Error handling is a necessity, but if it obsecures logic, it is wrong.
-
-//	Use exceptions rather than return codes
-
-//	Write try-catch-finally statement first
-
-//	Use unchecked exceptions
-
-//	Provide context with exceptions
-
-//	Defien exception class in terms of a caller's needs
-
-//	Define the normal flow
-
-//	Don't return null
-
-//	Don't pass null
-
 
 int main()
 {
@@ -37,5 +13,13 @@ int main()
 }
 
 //	Summary:
-//		
+//	Error handling is a necessity, but if it obscures logic, it is implemented incorrectly (recall, a function should do one thing - error handling is one thing)
+//	Use exceptions rather than return codes
+//	Write try-catch-finally statement first
+//	Use unchecked exceptions (don't specify exceptions in function signatures)
+//	Exceptions should provide a message with context
+//	Define exception class in terms of a caller's needs - create categories of exceptions where appropriate
+//	Define the normal flow (encapsulate special case behaviour instead of using exceptions where possible)
+//	Don't return null
+//	Don't pass null
 
